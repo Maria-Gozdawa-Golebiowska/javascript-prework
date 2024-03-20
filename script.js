9c177f15404191444bd9e9f061ee21080cc55764
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-03-20 21:46:31 +0100

    poprawa kodu projektu

--- script.js
+++ script.js
@@ -1,66 +1,70 @@
-function clearMessages() {
-  // Funkcja czyszcząca wiadomości
+{
+  const clearMessages = function() {
+    document.getElementById('messages').innerText = '';
+  };
+
+  const playGame = function(playerInput) {
+    clearMessages();
+    console.log('Gracz wpisał: ' + playerInput);
+  
+    const getPlayerMove = function() {
+      if (playerInput === '1') {
+        return 'kamień';
+      } else if (playerInput === '2') {
+        return 'papier';
+      } else if (playerInput === '3') {
+        return 'nożyce';
+      } else {
+        return 'nieznany ruch';
+      }
+    };
+    
+    const playerMoveResult = getPlayerMove();
+    console.log(playerMoveResult);
+    alert('Twój ruch to: ' + playerMoveResult);
+  
+    // komputer
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerMove;
+    if (randomNumber === 1) {
+      computerMove = 'kamień';
+    } else if (randomNumber === 2) {
+      computerMove = 'papier';
+    } else if (randomNumber === 3) {
+      computerMove = 'nożyce';
+    }
+  
+    console.log('Ruch komputera to: ' + computerMove);
+    alert('Ruch komputera to: ' + computerMove);
+  
+    if (playerMoveResult === 'nieznany ruch') {
+      console.log('Niepoprawy wybór gracza. Wybierz 1, 2 lub 3.');
+    } else if (playerMoveResult === computerMove) {
+      console.log('Remis');
+      alert('Remis');
+    } else if (
+      (playerMoveResult === 'kamień' && computerMove === 'nożyce') ||
+      (playerMoveResult === 'papier' && computerMove === 'kamień') ||
+      (playerMoveResult === 'nożyce' && computerMove === 'papier')
+    ) {
+      console.log('Gracz wygrywa!');
+      alert('Gracz wygrywa!');
+    } else {
+      console.log('Komputer wygrywa!');
+      alert('Komputer wygrywa!');
+    }
+  };
+  
+  // Wywołanie funkcji playGame 
+  document.getElementById('play-rock').addEventListener('click', function () {
+    playGame('1');
+  });
+  
+  document.getElementById('play-paper').addEventListener('click', function () {
+    playGame('2');
+  });
+  
+  document.getElementById('play-scissors').addEventListener('click', function () {
+    playGame('3');
+  });
 }
-
-function playGame(playerInput) {
-  clearMessages();
-
-  console.log('Gracz wpisał: ' + playerInput);
-
-  let playerMove = 'nieznany ruch';
-  if (playerInput == '1') {
-    playerMove = 'kamień';
-  } else if (playerInput == '2') {
-    playerMove = 'papier';
-  } else if (playerInput == '3') {
-    playerMove = 'nożyce';
-  }
-
-  console.log(playerMove);
-  alert('Twój ruch to: ' + playerMove);
-
-  // Losowanie ruchu komputera
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  let computerMove;
-  if (randomNumber == 1) {
-    computerMove = 'kamień';
-  } else if (randomNumber == 2) {
-    computerMove = 'papier';
-  } else if (randomNumber == 3) {
-    computerMove = 'nożyce';
-  }
-
-  console.log('Ruch komputera to: ' + computerMove);
-  alert('Ruch komputera to: ' + computerMove);
-
-  // Sprawdzenie wyniku gry
-  if (playerMove === 'nieznany ruch') {
-    console.log('Niepoprawny wybór gracza. Wybierz 1, 2 lub 3.');
-  } else if (playerMove === computerMove) {
-    console.log('Remis');
-    alert('Remis');
-  } else if (
-    (playerMove == 'kamień' && computerMove == 'nożyce') ||
-    (playerMove == 'papier' && computerMove == 'kamień') ||
-    (playerMove == 'nożyce' && computerMove == 'papier')
-  ) {
-    console.log('Gracz wygrywa!');
-    alert('Gracz wygrywa!');
-  } else {
-    console.log('Komputer wygrywa!');
-    alert('Komputer wygrywa!');
-  }
-}
-
-// Wywołanie funkcji playGame po kliknięciu odpowiedniego przycisku
-document.getElementById('play-rock').addEventListener('click', function () {
-  playGame('1');
-});
-
-document.getElementById('play-paper').addEventListener('click', function () {
-  playGame('2');
-});
-
-document.getElementById('play-scissors').addEventListener('click', function () {
-  playGame('3');
-});
